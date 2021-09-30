f3212bfdd684a97b4d98f6bef509bc62ca450fdc
Coodos <info@coodos.co>
2021-09-30 16:29:52 +0530

    added template support for credential templates

--- src/pages/user/applications/NewApplicationUser.tsx
+++ src/pages/user/applications/NewApplicationUser.tsx
@@ -1,125 +1,201 @@
-import {useState, useRef} from 'react'
-import styled, { withTheme } from 'styled-components'
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import styled, { withTheme } from "styled-components";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+import { ICredentialTemplate } from "../../../interfaces/credTemplate.interface";
 import {
-    CommonElementsUser,
-    PageContentContainer,
-    MainContentContainer,
-    MainContent,
-    Button,
-    Select,
-    InputText,
-    Hr
-} from "../../../components/ui"
+  CommonElementsUser,
+  PageContentContainer,
+  MainContentContainer,
+  MainContent,
+  Button,
+  Select,
+  InputBase,
+  Hr,
+} from "../../../components/ui";
 
 const CustomMainContent = styled(MainContent)`
-    display: grid;
-    grid-template-rows: repeat(3, auto);
-    fieldset {
-        width: 100%;
-    }
-    hr {
-        width: 100%;
-        margin-bottom: 3.3rem;
-    }
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  fieldset {
+    width: 100%;
+  }
+  hr {
+    width: 100%;
+    margin-bottom: 3.3rem;
+  }
 `;
 
 const Div1 = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 2rem;
-
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 2rem;
 `;
 
 const Div2 = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: repeat(6, auto);
-    grid-gap: 2rem;
-    padding-bottom: 2rem;
-    fieldset:nth-of-type(odd) {
-        grid-column: 1 / span 1;
-    }
-    fieldset:nth-of-type(even) {
-        grid-column: 2 / span 1;
-    }
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(6, auto);
+  grid-gap: 2rem;
+  padding-bottom: 2rem;
+  fieldset:nth-of-type(odd) {
+    grid-column: 1 / span 1;
+  }
+  fieldset:nth-of-type(even) {
+    grid-column: 2 / span 1;
+  }
 `;
 
 /**
-* Returns the NewApplicationUser component which is the page that 
-* contains the form to create a new application for the user.
-* @param {Object} theme - To receive the theme from the parent component. 
-* @returns {ReactElement} - The NewApplicationUser component
-*/
-const NewApplicationUser = ({theme}) => {
-    const userInputRefs = useRef({})
-    userInputRefs.current = {
-        appType: null,
-        appName: null,
-        firstName: null,
-        lastName: null,
-        businessName: null,
-        businessType: null,
-        address1: null,
-        address2: null,
-        city: null,
-        state: null,
-        postalCode: null,
-        country: null,
-        businessRegNo: null,
-        regAuthority: null
+ * Returns the NewApplicationUser component which is the page that
+ * contains the form to create a new application for the user.
+ * @param {Object} theme - To receive the theme from the parent component.
+ * @returns {ReactElement} - The NewApplicationUser component
+ */
+const NewApplicationUser = ({ theme }) => {
+  const userInputRefs = useRef({});
+
+  const [templates, setTemplates] = useState<any[]>();
+  const [applicationNames, setApplicationNames] = useState<any[]>();
+  const [template, setTemplate] = useState<ICredentialTemplate>();
+
+  userInputRefs.current = {
+    appType: null,
+    appName: null,
+    firstName: null,
+    lastName: null,
+    businessName: null,
+    businessType: null,
+    address1: null,
+    address2: null,
+    city: null,
+    state: null,
+    postalCode: null,
+    country: null,
+    businessRegNo: null,
+    regAuthority: null,
+  };
+
+  const userInfoMeta = useSelector((state: RootState) => state.userLogin);
+  const { userInfo }: any = userInfoMeta;
+
+  const handleTypeChange = async (e) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
     };
-
-    const [userInput, setUserInput] = useState({
-        appType: userInputRefs.current['appType'] ? userInputRefs.current['appType'].value : "",
-        appName: userInputRefs.current['appName'] ? userInputRefs.current['appName'].value : "",
-        firstName: userInputRefs.current['firstName'] ? userInputRefs.current['firstName'].value : "",
-        lastName: userInputRefs.current['lastName'] ? userInputRefs.current['lastName'].value : "",
-        businessName: userInputRefs.current['businessName'] ? userInputRefs.current['businessName'].value : "",
-        businessType: userInputRefs.current['businessType'] ? userInputRefs.current['businessType'].value : "",
-        address1: userInputRefs.current['address1'] ? userInputRefs.current['address1'].value : "",
-        address2: userInputRefs.current['address2'] ? userInputRefs.current['address2'].value : "",
-        city: userInputRefs.current['city'] ? userInputRefs.current['city'].value : "",
-        state: userInputRefs.current['state'] ? userInputRefs.current['state'].value : "",
-        postalCode: userInputRefs.current['postalCode'] ? userInputRefs.current['postalCode'].value : "",
-        country: userInputRefs.current['country'] ? userInputRefs.current['country'].value : "",
-        businessRegNo: userInputRefs.current['businessRegNo'] ? userInputRefs.current['businessRegNo'].value : "",
-        regAuthority: userInputRefs.current['regAuthority'] ? userInputRefs.current['regAuthority'].value : "",
-    });
-
-    const submitAppBtn = (
-        <Button primary btnColor={theme.btnPriBg}>SUBMIT APPLICATION</Button>
+    const { data } = await axios.get(
+      `/api/cred-templates?credentialType=${e.target.value}`,
+      config
     );
-    return (
-        <>
-        <CommonElementsUser menuActive="Applications" />
-        <PageContentContainer>
-            <MainContentContainer>
-                <CustomMainContent contentTitle="New Application" componentRight={submitAppBtn}>
-                    <Div1>
-                        <Select inputRef={el => userInputRefs.current['appType'] = el} placeholder="Application Type" optionList={["License"]} />
-                        <Select inputRef={el => userInputRefs.current['appName'] = el} placeholder="Application Name" optionList={["Animal Agriculture License"]} />  
-                    </Div1>
-                    <Hr />
-                    <Div2>
-                        <InputText inputRef={el => userInputRefs.current['firstName'] = el} placeholder="First Name" />
-                        <InputText inputRef={el => userInputRefs.current['lastName'] = el} placeholder="Last Name" />
-                        <InputText inputRef={el => userInputRefs.current['businessName'] = el} placeholder="Business Name" />
-                        <InputText inputRef={el => userInputRefs.current['businessType'] = el} placeholder="Business Type" />
-                        <InputText inputRef={el => userInputRefs.current['address1'] = el} placeholder="Address 1" />
-                        <InputText inputRef={el => userInputRefs.current['address2'] = el} placeholder="Address 2" />
-                        <InputText inputRef={el => userInputRefs.current['city'] = el} placeholder="City" />
-                        <InputText inputRef={el => userInputRefs.current['state'] = el} placeholder="State" />
-                        <InputText inputRef={el => userInputRefs.current['postalCode'] = el} placeholder="Postal Code" />
-                        <InputText inputRef={el => userInputRefs.current['country'] = el} placeholder="Country" />
-                        <InputText inputRef={el => userInputRefs.current['businessRegNo'] = el} placeholder="Business Regsitration Number" />
-                        <InputText inputRef={el => userInputRefs.current['regAuthority'] = el} placeholder="Regsitration Authority" />
-                    </Div2>
-                </CustomMainContent>
-            </MainContentContainer>
-        </PageContentContainer>
-        
-        </>
-    );
-}
+    setTemplates(data);
+  };
 
-export default withTheme(NewApplicationUser)
+  const handleCredChange = async (e) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(
+      `/api/cred-templates/find?name=${e.target.value}`,
+      config
+    );
+    setTemplate(data);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const data = {};
+    for (let i = 0; i < e.target.children.length; i++) {
+      const elem = e.target.children[i];
+      if (elem.placeholder && elem.value) {
+        data[elem.placeholder] = elem.value;
+      }
+    }
+    console.log(data);
+  };
+
+  useEffect(() => {
+    if (!templates || templates.length === 0) return;
+    const names = templates.map((temp) => temp.name);
+    setApplicationNames(names);
+  }, [templates]);
+
+  const submitAppBtn = (
+    <Button primary btnColor={theme.btnPriBg}>
+      SUBMIT APPLICATION
+    </Button>
+  );
+  return (
+    <>
+      <CommonElementsUser menuActive="Applications" />
+      <PageContentContainer>
+        <MainContentContainer>
+          <CustomMainContent
+            contentTitle="New Application"
+            componentRight={<div></div>}
+          >
+            <Div1>
+              <Select
+                inputRef={(el) => (userInputRefs.current["appType"] = el)}
+                placeholder="Application Type"
+                optionList={["License", "Certificate", "Ticket"]}
+                onChange={handleTypeChange}
+              />
+              {templates && applicationNames && (
+                <Select
+                  placeholder="Application Name"
+                  optionList={applicationNames}
+                  onChange={handleCredChange}
+                />
+              )}
+            </Div1>
+            <Hr />
+            <Div2>
+              <form onSubmit={handleFormSubmit} id="application-form">
+                {template &&
+                  template.fields.map((field, index) => (
+                    <input
+                      name={String(field.label)}
+                      type={field.type}
+                      placeholder={String(field.label)}
+                      style={{
+                        width: "100%",
+                        height: "3rem",
+                        padding: "0 2.5rem 0.2rem 1rem",
+                        borderRadius: "30px",
+                        boxShadow: "none",
+                        border: "1px solid #E0E0E0",
+                        boxSizing: "border-box",
+                        fontSize: "1rem",
+                        outline: "none",
+                        color: "#353535",
+                        margin: "10px 0",
+                        marginBottom: `${
+                          template.fields.length - 1 === index ? "40" : "10"
+                        }px`,
+                      }}
+                    />
+                  ))}
+                <div>
+                  {template && template.fields && (
+                    <Button primary btnColor={theme.btnPriBg}>
+                      SUBMIT APPLICATION
+                    </Button>
+                  )}
+                </div>
+              </form>
+            </Div2>
+          </CustomMainContent>
+        </MainContentContainer>
+      </PageContentContainer>
+    </>
+  );
+};
+
+export default withTheme(NewApplicationUser);
